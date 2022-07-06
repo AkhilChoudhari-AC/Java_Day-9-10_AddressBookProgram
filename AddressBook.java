b9335f3af4dee628b49d7ea4465f90ccfb5e70a5
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-06 20:45:51 +0530

    START:Welcome to the Address Book System Program

---
+++ AddressBook.java
@@ -0,0 +1,8 @@
+package AddressBookProgram;
+
+public class AddressBook {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Address Book System Program");
+    }
+}
